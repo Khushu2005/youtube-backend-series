0439bbc57044f9172e3ec1c9ec450234a94177c8
Khushi sharma <ks0541929@gmail.com>
2026-01-07 19:52:25 +0530

    Added login api

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -42,4 +42,34 @@ error: error.message});
 }
 };
 
-module.exports={registerUser}
+async function loginUser (req,res){
+
+try {
+//fetching from frontend
+const { email, password } = req.body;
+
+// finding user with email
+const user = await userModel.findOne({ email });
+
+// if user not found return error
+if (!user) {
+ return res.status(400).json({ message: "No user found " });}
+
+// comparing password
+ const isPasswordMatched = await bcrypt.compare(password, user.password);
+        
+//  if password not matched return error
+if (!isPasswordMatched) {
+ return res.status(400).json({ message: "Invalid credentials " });}
+
+//   if all ok send response
+res.status(200).json({
+message: "Login Successful ",
+user: user });
+
+ } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports={registerUser , loginUser}
